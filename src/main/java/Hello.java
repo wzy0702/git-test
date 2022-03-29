3d1f05b7e07ce388ddcf27739a85edba8d1c8c2d
wzy <2856423181@qq.com>
2022-03-29 22:35:43 +0800

    init

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * @MethodName $
+ * @Description
+ * @Author wzy
+ * @create 2022-03-29 22:28
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello........");
+        System.out.println("push test");
+    }
+}
